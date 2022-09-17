44f08959ab4eb9f5988cbfc15eaac62e55d437bd
Daniil-Vladimirov <antebellum2022@gmail.com>
2022-09-17 11:28:53 +0300

    Update2

--- build.gradle.kts
+++ build.gradle.kts
@@ -12,8 +12,7 @@ plugins {
 group = "com.example"
 version = "0.0.1"
 
-application {
-    mainClass.set("io.ktor.server.netty.EngineMain")
+application {    mainClass.set("io.ktor.server.netty.EngineMain")
 
 }
 tasks.create("stage"){
